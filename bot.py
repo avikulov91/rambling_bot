915b193ce1905bf28117ad511eb0376a110adb19
avikulov91 <avikulov91@gmail.com>
2025-09-19 02:12:04 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -219,5 +219,14 @@ def set_webhook():
     return f"Webhook установлен: {url}", 200
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "8081"))
+    import sys
+
+    # Порт по умолчанию
+    port = 8080
+
+    # Если передан аргумент --port=XXXX → берём его
+    for arg in sys.argv[1:]:
+        if arg.startswith("--port="):
+            port = int(arg.split("=", 1)[1])
+
     app.run(host="0.0.0.0", port=port)
